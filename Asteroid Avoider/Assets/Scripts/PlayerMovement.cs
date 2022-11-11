e361ec86c16b3503b0e231c7c7aec6ee1cc82997
eliebeaino <eliebeaino89@gmail.com>
2022-11-11 07:43:47 +0200

    Update Player Movement
    - added screen wraparound
    - added player rotation

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float forceMagnitude = 1f;
     [SerializeField] private float maxVelocity = 10f;
+    [SerializeField] private float rotationSpeed = 5f;
 
     private Camera mainCam;
     private Rigidbody rb;
@@ -19,8 +21,22 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-
     void Update()
+    {
+        PrcoessInput();
+        KeepPlayerOnScreen();
+        RotateToFaceVelocity();
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero) { return; }
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+
+    private void PrcoessInput()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
@@ -38,11 +54,37 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void KeepPlayerOnScreen()
     {
-        if (movementDirection == Vector3.zero) { return; }
-        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = mainCam.WorldToViewportPoint(transform.position);
 
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        if(viewportPosition.x >1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        if (viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
+    }
+
+    private void RotateToFaceVelocity()
+    {
+        if (rb.velocity == Vector3.zero) { return; }
+
+        Quaternion newRotation = Quaternion.LookRotation(rb.velocity, Vector3.back);
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
     }
 }
